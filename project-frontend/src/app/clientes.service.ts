26992473a1b687d595a12b0f92443dde7df741ed
guillermodolan <guillermo230899@gmail.com>
2022-08-24 09:10:16 -0300

    Agregado de modelo de ropa.

--- project-frontend/src/app/clientes.service.ts
+++ project-frontend/src/app/clientes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ClienteReqResResponse } from './models/cliente-reqres-response';
- 
+
 @Injectable({
   providedIn: 'root'
 })
---
+++ project-frontend/src/app/models/ropa.model.ts
@@ -0,0 +1,9 @@
+//Modelo creado para las entidades de ropa
+
+export class Ropa{
+    
+    //Sintaxis simplificada, en la cual dentro del constructor declaramos e inicializamos los atributos.
+    //Al poner 'public' se crea y se declara el atributo, como parte del atributo de nuestra clase.
+    constructor(public marca: string, public categoria: string, public talle: string, 
+                public detalle: string, public tipoRopa: number, public temporada: number) {}
+}
