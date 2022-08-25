83480c18cbe2eda975f2488a397fcd4974fe7d31
guillermodolan <guillermo230899@gmail.com>
2022-08-25 17:45:41 -0300

    Función de 'Agregar ropa' agregada correctamente

--- project-frontend/src/app/models/ropa.model.ts
+++ project-frontend/src/app/models/ropa.model.ts
@@ -5,5 +5,9 @@ export class Ropa{
     //Sintaxis simplificada, en la cual dentro del constructor declaramos e inicializamos los atributos.
     //Al poner 'public' se crea y se declara el atributo, como parte del atributo de nuestra clase.
     constructor(public marca: string, public categoria: string, public talle: string, 
-                public detalle: string, public tipoRopa: number, public temporada: number) {}
+                public detalle: string, public tipoRopa: string, 
+                public temporada: string, public precioRopa: string, public _id?: string) {}
+
+
+    
 }
